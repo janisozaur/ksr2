1e47c131923a7cead8d554bb776301ac1c0c0be2
mariusz <mariusz321@gmail.com>
2012-01-15 22:28:18 +0100

    sprawozdnaie - badani, dyskusja, wnioski, wylosowany zbior 10000 rekordow z bazy danych uzupelnione kwantyfikatory i zmienne lingwistyczne

--- db_summarization/main.cpp
+++ db_summarization/main.cpp
@@ -94,7 +94,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QString dataFilePath = "database.dat";
+    QString dataFilePath = "database10000.dat";
     QString dataDescFilePath = "data_description.txt";
 
 
@@ -115,38 +115,6 @@ int main(int argc, char *argv[])
     qDebug()<<"Liczba zmiennych lingwistycznych: "<< linguisticValMap.size();
     qDebug()<<"Liczba wartosci lingwistycznych: "<< valuesCount;
 
-
-
-//    QList<const LinguisticValue *> summarizers =  parser.parseLinguisticValues("fuzzy_sets_lingValues.txt").value(19);
-//    qDebug()<<"Liczba wartosci lingwistycznych: "<< summarizers.size();
-
- //   QList<const LinguisticValue *> qualifiers = QList< const LinguisticValue*>();
-
-//    qDebug()<<"T1: "<<QualityMeasures::computeT1(quantifierList.at(0), qualifiers, summarizers, dbRows);
-//    qDebug()<<"T2: "<<QualityMeasures::computeT2T9(summarizers, dbRows);
-//    qDebug()<<"T3: "<<QualityMeasures::computeT3(qualifiers, summarizers, dbRows);
-//    qDebug()<<"T4: "<<QualityMeasures::computeT4(qualifiers, summarizers, dbRows);
-//    qDebug()<<"T5: "<<QualityMeasures::computeT5T11(summarizers.size());
-//    qDebug()<<"T6: "<<QualityMeasures::computeT6(quantifierList.at(0), dbRows.size());
-//    qDebug()<<"T7: "<<QualityMeasures::computeT7(quantifierList.at(0), dbRows.size());
-//    qDebug()<<"T8: "<<QualityMeasures::computeT8T10(summarizers, dbRows);
-//    qDebug()<<"T9: "<<QualityMeasures::computeT2T9(qualifiers, dbRows);
-//    qDebug()<<"T10: "<<QualityMeasures::computeT8T10(qualifiers, dbRows);
-//    qDebug()<<"T11: "<<QualityMeasures::computeT5T11(qualifiers.size());
-
-//    QList<QualityMeasures::Measures> measures;
-//    measures << QualityMeasures::T1	<< QualityMeasures::T2 << QualityMeasures::T3
-//             << QualityMeasures::T4 << QualityMeasures::T5 << QualityMeasures::T6
-//             << QualityMeasures::T7 << QualityMeasures::T8 << QualityMeasures::T9
-//             << QualityMeasures::T10 << QualityMeasures::T11;
-//    QList<double> ts = QualityMeasures::computeT(measures, quantifierList.at(0),
-//                                                 qualifiers, summarizers, dbRows);
-//    for (int i = 0; i < measures.size(); i++) {
-//        const QString tIdStr = QString::number(int(measures.at(i)));
-//        const QString tValStr = QString::number(ts.at(i));
-//        qDebug() << QString("T%1: %2").arg(tIdStr, tValStr);
-//    }
-
     QMap<QString, double> weightsMap = QMap<QString, double>();
     weightsMap.insert("T1", 1);
     weightsMap.insert("T2", 1);
@@ -160,13 +128,25 @@ int main(int argc, char *argv[])
     weightsMap.insert("T10", 1);
     weightsMap.insert("T11", 1);
 
+//    QMap<QString, double> weightsMap = QMap<QString, double>();
+//    weightsMap.insert("T1", 1);
+//    weightsMap.insert("T2", 1);
+//    weightsMap.insert("T3", 1);
+//    weightsMap.insert("T4", 1);
+//    weightsMap.insert("T5", 1);
+//    weightsMap.insert("T6", 0);
+//    weightsMap.insert("T7", 0);
+//    weightsMap.insert("T8", 0);
+//    weightsMap.insert("T9", 0);
+//    weightsMap.insert("T10", 0);
+//    weightsMap.insert("T11", 0);
+
+
     int res = 0;
-    res = testType2FuzzySet(a);
+        //res = testType2FuzzySet(a);
     if (res < 0) {
         return res;
     }
-
-    //qDebug()<<"Total: "<<QualityMeasures::computeTotalQuality(weightsMap, quantifierList.at(0), qualifiers, summarizers, dbRows);
 
     QElapsedTimer timer;
     timer.start();
