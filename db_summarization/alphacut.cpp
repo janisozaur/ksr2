b54b61744f744afb857082cd71a654b830d70791
mariusz <mariusz321@gmail.com>
2012-01-23 10:54:17 +0100

    poprawiony alphaCut

--- db_summarization/alphacut.cpp
+++ db_summarization/alphacut.cpp
@@ -3,14 +3,15 @@
 
 AlphaCut::AlphaCut()
 {
+    alpha = 0;
 }
 
 double AlphaCut::membership(const QVariant &element) const {
-    return fuzzySet->membership(element) > 0 ? 1 : 0;
+    return fuzzySet->membership(element) > alpha ? 1 : 0;
 }
 
 double AlphaCut::membership(const QVector<QVariant> &elements) const {
-	return fuzzySet->membership(elements) > 0 ? 1 : 0;
+        return fuzzySet->membership(elements) > alpha ? 1 : 0;
 }
 
 FuzzySet const *AlphaCut::getFuzzySet() const {
@@ -18,4 +19,11 @@ FuzzySet const *AlphaCut::getFuzzySet() const {
 }
 void AlphaCut::setFuzzySet(const FuzzySet *fuzzySet){
     this->fuzzySet = fuzzySet;
+}
+
+const double AlphaCut::getAlpha() const{
+    return alpha;
+}
+void AlphaCut::setAlpha(const double alpha){
+    this->alpha = alpha;
 }
--- db_summarization/alphacut.h
+++ db_summarization/alphacut.h
@@ -8,12 +8,15 @@ class AlphaCut : public FuzzySet
 {
 private:
 	const FuzzySet * fuzzySet;
+        double alpha;
 public:
     AlphaCut();
 	double membership(const QVariant &element) const;
 	double membership(const QVector<QVariant> &elements) const;
 	const FuzzySet *getFuzzySet() const;
 	void setFuzzySet(const FuzzySet *fuzzySet);
+        const double getAlpha() const;
+        void setAlpha(const double alpha);
 };
 
 #endif // ALPHACUT_H
